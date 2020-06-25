574c46f6c0fe428e317d5a38622fc6070877bd7e
Dave Collier <lardelbow@gmail.com>
2020-06-25 12:23:30 +0100

    Initial commit

---
+++ src/main/java/com/docswebapps/msazuresandpit/MsazureSandpitApplication.java
@@ -0,0 +1,13 @@
+package com.docswebapps.msazuresandpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsazureSandpitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsazureSandpitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docswebapps/msazuresandpit/MsazureSandpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docswebapps.msazuresandpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsazureSandpitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
