06d3190887cf0e2c5540f4367a5cfd692d781edd
Dave Collier <lardelbow@gmail.com>
2020-06-30 13:12:14 +0100

    Basic Azure file access working

--- src/main/java/com/docswebapps/msazuresandpit/web/rest/StorageContainerRestController.java
+++ src/main/java/com/docswebapps/msazuresandpit/web/rest/StorageContainerRestController.java
@@ -2,10 +2,15 @@ package com.docswebapps.msazuresandpit.web.rest;
 
 import com.docswebapps.msazuresandpit.service.StorageContainerService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @RestController
 @RequestMapping("/api/v1/storage/container")
@@ -13,13 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class StorageContainerRestController {
     private final StorageContainerService storageContainerService;
 
+    @Value("${azureimageblob}")
+    private Resource blobFile;
+
     public StorageContainerRestController(StorageContainerService storageContainerService) {
         this.storageContainerService = storageContainerService;
     }
 
-    @PostMapping("/create")
-    public void createNewContainer(@RequestParam("Name") String containerName) {
-        log.info("StorageContainerRestController: createNewContainer() called with container name: {}", containerName);
-        this.storageContainerService.createContainer(containerName);
+//    @PostMapping("/create")
+//    public void createNewContainer(@RequestParam("Name") String containerName) {
+//        log.info("StorageContainerRestController: createNewContainer() called with container name: {}", containerName);
+//        this.storageContainerService.createContainer(containerName);
+//    }
+
+
+    @GetMapping("/public-images/azure")
+    public String readBlobFile() throws IOException {
+        return StreamUtils.copyToString(
+                this.blobFile.getInputStream(),
+                Charset.defaultCharset());
     }
+
+//    @PostMapping
+//    public String writeBlobFile(@RequestBody String data) throws IOException {
+//        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()) {
+//            os.write(data.getBytes());
+//        }
+//        return "file was updated";
+//    }
 }
